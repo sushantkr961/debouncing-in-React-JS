965044c61a416597a4c2ae21fb1eace08a553531
sushantkr961 <sushantonly961@gmail.com>
2022-11-11 14:10:39 +0530

    ok

---
+++ moviewebsite-master/src/context.js
@@ -0,0 +1,21 @@
+import React, { useContext, useState } from "react";
+import useFetch from "./useFetch";
+
+const AppContext = React.createContext();
+
+const AppProvider = ({ children }) => {
+  const [query, setQuery] = useState("hacker");
+  const { isLoading, isError, movie } = useFetch(`&s=${query}`);
+
+  return (
+    <AppContext.Provider value={{ query, movie, setQuery, isLoading, isError }}>
+      {children}
+    </AppContext.Provider>
+  );
+};
+
+const useGlobalContext = () => {
+  return useContext(AppContext);
+};
+
+export { AppContext, AppProvider, useGlobalContext };
